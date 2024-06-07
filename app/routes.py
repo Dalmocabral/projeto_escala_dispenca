be50c94864af773a304c0de65114a6aec46730fb
Dalmo dos Santos Cabral <dalmo.santos.cabral@gmail.com>
2024-06-07 11:32:07 -0300

    commit

--- app/routes.py
+++ app/routes.py
@@ -2,15 +2,19 @@ from flask import render_template, redirect, url_for
 from app import app, db
 from app.models import User
 from datetime import datetime
+from sqlalchemy import case
 
 @app.route('/')
 def index():
     users = User.query.order_by(
         User.afastado.asc(),  # Primeiro os não afastados
-        User.dataDispensa.asc().nullsfirst()  # Depois os que não têm data de dispensa (nullsfirst faz com que nulls venham primeiro)
+        case(
+            (User.dataDispensa == None, 1),
+            else_=0
+        ).asc(),
+        User.dataDispensa.asc()
     ).all()
     return render_template('index.html', users=users)
-
 
 @app.route('/dispensa/<int:user_id>', methods=['POST'])
 def dispensa(user_id):
